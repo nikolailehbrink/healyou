4075b8af063461c5d859455d308611c6a6cdc547
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2023-12-19 16:44:58 +0100

    refactor: extract ChatForm to own component

--- app/diagnose/page.tsx
+++ app/diagnose/page.tsx
@@ -1,11 +1,9 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { Textarea } from "@/components/ui/textarea";
 import { cn } from "@/lib/utils";
-import { PaperPlaneTilt } from "@phosphor-icons/react";
 import { useChat } from "ai/react";
 import DiagnoseForm from "./form";
+import ChatForm from "@/components/ChatForm";
 
 export default function DiagnosePage() {
   const { messages, input, handleInputChange, handleSubmit, isLoading } =
@@ -36,34 +34,14 @@ export default function DiagnosePage() {
       </div>
       <div className="sticky bottom-4 mb-4 flex justify-center">
         <div className="absolute inset-0 -bottom-4 -top-12 bg-gradient-to-b from-transparent to-neutral-900"></div>
-        <div className="relative flex max-w-2xl flex-grow items-center gap-x-2 self-center">
-          {messages.length > 0 ? (
-            <>
-              <form onSubmit={handleSubmit}>
-                <Textarea
-                  value={input}
-                  onChange={handleInputChange}
-                  placeholder="Say something"
-                  rows={1}
-                  className="rounded-br- rounded-3xl px-4"
-                />
-                <Button
-                  disabled={isLoading}
-                  type="submit"
-                  className="flex-shrink-0 text-neutral-950 hover:bg-primary"
-                  variant="default"
-                  size="icon"
-                >
-                  <PaperPlaneTilt size={24} weight="duotone" />
-                </Button>
-              </form>
-            </>
-          ) : (
-            <>
-              <DiagnoseForm />
-            </>
-          )}
-        </div>
+        {messages.length > 0 && (
+          <ChatForm
+            input={input}
+            handleInputChange={handleInputChange}
+            handleSubmit={handleSubmit}
+            isLoading={isLoading}
+          />
+        )}
       </div>
     </>
   );
---
+++ components/ChatForm.tsx
@@ -0,0 +1,52 @@
+import { CircleNotch, PaperPlaneTilt } from "@phosphor-icons/react/dist/ssr";
+import { Button } from "./ui/button";
+import { Textarea } from "./ui/textarea";
+import type { ChangeEvent, FormEvent } from "react";
+import type { ChatRequestOptions } from "ai";
+
+type Props = {
+  input: string;
+  handleInputChange: (
+    e: ChangeEvent<HTMLTextAreaElement> | ChangeEvent<HTMLInputElement>,
+  ) => void;
+  handleSubmit: (
+    e: FormEvent<HTMLFormElement>,
+    chatRequestOptions?: ChatRequestOptions | undefined,
+  ) => void;
+  isLoading: boolean;
+};
+
+export default function ChatForm({
+  input,
+  handleInputChange,
+  handleSubmit,
+  isLoading,
+}: Props) {
+  return (
+    <form
+      className="relative flex max-w-2xl flex-grow items-center gap-x-2 self-center"
+      onSubmit={handleSubmit}
+    >
+      <Textarea
+        value={input}
+        onChange={handleInputChange}
+        placeholder="Say something"
+        rows={1}
+        className="rounded-br- rounded-3xl px-4"
+      ></Textarea>
+      <Button
+        disabled={isLoading}
+        type="submit"
+        className="flex-shrink-0 text-neutral-950 hover:bg-primary"
+        variant="default"
+        size="icon"
+      >
+        {isLoading ? (
+          <CircleNotch className="animate-spin" size={24} weight="duotone" />
+        ) : (
+          <PaperPlaneTilt size={24} weight="duotone" />
+        )}
+      </Button>
+    </form>
+  );
+}
