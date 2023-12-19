3d253cfcda726591c8558278ab6dd4d0ca9c8027
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2023-12-19 16:40:10 +0100

    feat(diagnose page): update gpt model and system message

--- app/api/diagnose/route.ts
+++ app/api/diagnose/route.ts
@@ -14,7 +14,7 @@ export async function POST(req: Request) {
 
   // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
+    model: "gpt-3.5-turbo-1106",
     stream: true,
     messages: [
       {
@@ -24,7 +24,7 @@ export async function POST(req: Request) {
         During the interaction, you should only mention the need to consult a doctor once at the end of the consultation instead of repeating this several times. 
         The aim is to provide a realistic preliminary diagnosis based on the information provided by the user, with a professional description of the potential injury or illness. 
         You should avoid making assumptions or giving advice based on incomplete information to ensure accurate and relevant assessments. 
-        Do not give advice on how to cure the injury or illness.`,
+        Do not give advice on how to cure the injury or illness. Instead refer to the therapy site which is accessible via the navigation menu. Answer in Markdown and format the answer in a way that is easy to read. Also the final detection has to be marked with a bold text.`,
       },
       ...messages,
     ],
