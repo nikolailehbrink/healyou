13c4212ec0025d784e0a022e0a1cd83bd5f35878
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2023-12-21 11:25:24 +0100

    style: revamp login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,24 @@
-import Link from "next/link";
 import { headers, cookies } from "next/headers";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  FingerprintSimple,
+  GithubLogo,
+  GoogleLogo,
+  LinkedinLogo,
+  Signature,
+} from "@phosphor-icons/react/dist/ssr";
 
 export default async function Login({
   searchParams,
@@ -19,13 +36,15 @@ export default async function Login({
     return redirect("/");
   }
 
-  const signIn = async (formData: FormData) => {
+  const signInWithEmailAndPassword = async (formData: FormData) => {
     "use server";
 
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
+    const email = formData.get("email-login") as string;
+    const password = formData.get("password-login") as string;
     const cookieStore = cookies();
     const supabase = createClient(cookieStore);
+
+    console.log(email, password);
 
     const { error } = await supabase.auth.signInWithPassword({
       email,
@@ -33,10 +52,45 @@ export default async function Login({
     });
 
     if (error) {
+      console.log(error);
+
       return redirect("/login?message=Could not authenticate user");
     }
 
     return redirect("/");
+  };
+
+  const signUpWithEmailAndPassword = async (formData: FormData) => {
+    "use server";
+
+    const origin = headers().get("origin");
+    const email = formData.get("email-signup") as string;
+    const confirmEmail = formData.get("email-confirm-signup") as string;
+    const password = formData.get("password-signup") as string;
+    const cookieStore = cookies();
+    const supabase = createClient(cookieStore);
+
+    if (email !== confirmEmail) {
+      return redirect(
+        "/login?message=Both emails have to contain the same adress.",
+      );
+    }
+
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: `${origin}/auth/callback`,
+      },
+    });
+
+    console.log(error?.message);
+
+    if (error) {
+      return redirect(`/login?message=${error.message}`);
+    }
+
+    return redirect("/login?message=Check email to continue sign in process");
   };
 
   const signInWithGoogle = async () => {
@@ -105,103 +159,139 @@ export default async function Login({
     return redirect(data.url);
   };
 
-  const signUp = async (formData: FormData) => {
-    "use server";
-
-    const origin = headers().get("origin");
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
-    const cookieStore = cookies();
-    const supabase = createClient(cookieStore);
-
-    const { error } = await supabase.auth.signUp({
-      email,
-      password,
-      options: {
-        emailRedirectTo: `${origin}/auth/callback`,
-      },
-    });
-
-    console.log(error?.message);
-
-    if (error) {
-      return redirect(`/login?message=${error.message}`);
-    }
-
-    return redirect("/login?message=Check email to continue sign in process");
-  };
-
   return (
-    <div className="flex w-full flex-1 flex-col justify-center gap-2 px-8 sm:max-w-md">
-      <Link
-        href="/"
-        className="bg-btn-background hover:bg-btn-background-hover group absolute left-8 top-8 flex items-center rounded-md px-4 py-2 text-sm text-foreground no-underline"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
+    <div className="flex flex-grow flex-col justify-center gap-4 self-center">
+      <div className="flex items-stretch gap-2 text-primary">
+        <form
+          className="flex w-full items-center justify-center rounded-lg bg-neutral-950 p-2"
+          action={signInWithGoogle}
         >
-          <polyline points="15 18 9 12 15 6" />
-        </svg>{" "}
-        Back
-      </Link>
-
-      <form action={signInWithGoogle}>
-        <button>Zu Google</button>
-      </form>
-      <form action={signInWithLinkedIn}>
-        <button>Zu LinkedIn</button>
-      </form>
-      <form action={signInWithGitHub}>
-        <button>Zu GitHub</button>
-      </form>
-
-      <form
-        className="flex w-full flex-1 flex-col justify-center gap-2 text-foreground animate-in"
-        action={signIn}
-      >
-        <label className="text-md" htmlFor="email">
-          Email
-        </label>
-        <input
-          className="mb-6 rounded-md border bg-inherit px-4 py-2"
-          name="email"
-          placeholder="you@example.com"
-          required
-        />
-        <label className="text-md" htmlFor="password">
-          Password
-        </label>
-        <input
-          className="mb-6 rounded-md border bg-inherit px-4 py-2"
-          type="password"
-          name="password"
-          placeholder="••••••••"
-          required
-        />
-        <button className="mb-2 rounded-md bg-green-700 px-4 py-2 text-foreground">
-          Sign In
-        </button>
-        <button
-          formAction={signUp}
-          className="mb-2 rounded-md border border-foreground/20 px-4 py-2 text-foreground"
+          <button>
+            <GoogleLogo size={40} weight="duotone" />
+          </button>
+        </form>
+        <form
+          action={signInWithLinkedIn}
+          className="flex w-full items-center justify-center rounded-lg bg-neutral-950 p-2"
         >
-          Sign Up
-        </button>
-        {searchParams?.message && (
-          <p className="mt-4 bg-foreground/10 p-4 text-center text-foreground">
-            {searchParams.message}
-          </p>
-        )}
-      </form>
+          <button>
+            <LinkedinLogo size={40} weight="duotone" />
+          </button>
+        </form>
+        <form
+          action={signInWithGitHub}
+          className="flex w-full items-center justify-center rounded-lg bg-neutral-950 p-2"
+        >
+          <button>
+            <GithubLogo size={40} weight="duotone" />
+          </button>
+        </form>
+      </div>
+      <div className="flex items-center gap-2">
+        <hr className="flex-grow border-muted-foreground/50" />
+        <span className="text-sm text-muted-foreground">
+          Or with email and password
+        </span>
+        <hr className="flex-grow border-muted-foreground/50" />
+      </div>
+
+      <Tabs defaultValue="login" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="login">Login</TabsTrigger>
+          <TabsTrigger value="signup">Sign Up</TabsTrigger>
+        </TabsList>
+        <TabsContent value="login">
+          <form action={signInWithEmailAndPassword}>
+            <Card>
+              <CardHeader>
+                <CardDescription>
+                  Login with your email and password. <br /> Thank you for using
+                  the service!
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <div className="space-y-1">
+                  <Label htmlFor="email-login">Email</Label>
+                  <Input
+                    name="email-login"
+                    type="email"
+                    placeholder="john.doe@example.com"
+                    required
+                  />
+                </div>
+                <div className="space-y-1">
+                  <Label htmlFor="password-login">Password</Label>
+                  <Input
+                    type="password"
+                    name="password-login"
+                    placeholder="••••••••"
+                    required
+                  />
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Button>
+                  {" "}
+                  <FingerprintSimple size={24} weight="duotone" />
+                  Login{" "}
+                </Button>
+              </CardFooter>
+            </Card>
+          </form>
+        </TabsContent>
+        <TabsContent value="signup">
+          <form action={signUpWithEmailAndPassword}>
+            <Card>
+              <CardHeader>
+                <CardDescription>
+                  Sign up with your email and password. <br /> Thank you for
+                  using the service!
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <div className="space-y-1">
+                  <Label htmlFor="email-signup">Email</Label>
+                  <Input
+                    id="email-signup"
+                    type="email"
+                    placeholder="john.doe@example.com"
+                    required
+                  />
+                </div>
+                <div className="space-y-1">
+                  <Label htmlFor="email-confirm-signup">Confirm email</Label>
+                  <Input
+                    id="email-confirm-signup"
+                    type="email"
+                    placeholder="john.doe@example.com"
+                    required
+                  />
+                </div>
+                <div className="space-y-1">
+                  <Label htmlFor="password-signup">Password</Label>
+                  <Input
+                    type="password"
+                    name="password-signup"
+                    placeholder="••••••••"
+                    required
+                  />
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Button>
+                  {" "}
+                  <Signature size={24} weight="duotone" /> Sign Up
+                </Button>
+              </CardFooter>
+            </Card>
+          </form>
+        </TabsContent>
+      </Tabs>
+      {searchParams?.message && (
+        <p className="mt-4 bg-foreground/10 p-4 text-center text-foreground">
+          {searchParams.message}
+        </p>
+      )}
     </div>
   );
 }
