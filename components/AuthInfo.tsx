d527925a0360c11516d72bfd43ad737e071963d5
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2023-12-21 12:25:13 +0100

    fix: redirect to login page if user logs out

--- components/AuthInfo.tsx
+++ components/AuthInfo.tsx
@@ -33,7 +33,7 @@ export default async function AuthInfo() {
 
     const { error } = await supabase.auth.signOut();
     console.log(error);
-    redirect("/");
+    redirect("/login");
   }
 
   return user ? (
