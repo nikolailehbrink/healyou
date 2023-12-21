b273514f898fe2ad2e0906c28dc882758e8de441
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2023-12-21 15:23:14 +0100

    feat: add spreaded props to components

--- components/InfoBanner.tsx
+++ components/InfoBanner.tsx
@@ -1,8 +1,11 @@
 import { Info } from "@phosphor-icons/react/dist/ssr";
 
-export default function InfoBanner() {
+export default function InfoBanner({ ...props }) {
   return (
-    <div className="mx-auto flex max-w-3xl items-center gap-2 rounded-full bg-[hsl(21,100%,66%)] px-3 py-2 text-sm text-background">
+    <div
+      {...props}
+      className="mx-auto flex max-w-3xl items-center gap-2 rounded-full bg-[hsl(21,100%,66%)] px-3 py-2 text-sm text-background"
+    >
       <Info weight="duotone" size={20} />
       <p>
         This site is for initial guidance only, not a substitute for a
--- components/SocialLoginButton.tsx
+++ components/SocialLoginButton.tsx
@@ -4,9 +4,14 @@ type Props = {
   formAction: () => Promise<never>;
   icon: ReactNode;
 };
-export default function SocialLoginButton({ formAction, icon }: Props) {
+export default function SocialLoginButton({
+  formAction,
+  icon,
+  ...props
+}: Props) {
   return (
     <button
+      {...props}
       formAction={formAction}
       className="flex grow justify-center rounded-lg border-2 border-muted bg-neutral-950 p-2 hover:border-primary hover:bg-neutral-900"
     >
