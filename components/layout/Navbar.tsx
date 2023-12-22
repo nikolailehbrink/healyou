82ac43fcc66c6d35f8b025cba72f5c6bdbd8f98b
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2023-12-22 11:37:11 +0100

    feat: show nav items only if user is logged in

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -2,10 +2,11 @@ import { NavItems } from "@/data/Navbar";
 import Link from "next/link";
 import { Button } from "../ui/button";
 import Image from "next/image";
-import { UserCircle } from "@phosphor-icons/react/dist/ssr";
 import AuthInfo from "../AuthInfo";
+import { getUser } from "@/utils/supabase/server";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const user = await getUser();
   return (
     <nav className="sticky top-0 z-50 flex items-center py-4">
       <div className="absolute inset-0 -bottom-4 bg-gradient-to-b from-neutral-900 to-transparent"></div>
@@ -16,22 +17,24 @@ export default function Navbar() {
         </Link>
       </div>
 
-      <menu className="relative flex gap-x-2">
-        {NavItems.map(({ href, name, icon }) => (
-          <li key={href}>
-            <Button
-              className="font-inria text-base"
-              variant={"outline"}
-              asChild
-            >
-              <Link href={href}>
-                {icon}
-                {name}
-              </Link>
-            </Button>
-          </li>
-        ))}
-      </menu>
+      {user && (
+        <menu className="relative flex gap-x-2">
+          {NavItems.map(({ href, name, icon }) => (
+            <li key={href}>
+              <Button
+                className="font-inria text-base"
+                variant={"outline"}
+                asChild
+              >
+                <Link href={href}>
+                  {icon}
+                  {name}
+                </Link>
+              </Button>
+            </li>
+          ))}
+        </menu>
+      )}
       <div className="relative flex flex-1 justify-end">
         <AuthInfo />
       </div>
